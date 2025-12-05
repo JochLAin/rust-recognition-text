06df399b96368271890a4365654aa2b6246984b5
Jocelyn Faihy <jocelyn.faihy@gmail.com>
2025-12-05 08:02:59 +0100

    :construction: add graphs

--- packages/core/src/main.rs
+++ packages/core/src/main.rs
@@ -1,45 +1,9 @@
 use candle_core::{Device};
-use eframe::egui;
-use egui_plot::{Plot, Line, PlotPoints};
 use neural::{Activation, Network};
 
 mod dataset;
+mod frame;
 pub mod neural;
-
-struct App {
-  x: Vec<f64>,
-  y: Vec<f64>,
-}
-
-impl eframe::App for App {
-  fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-    egui::CentralPanel::default().show(ctx, |ui| {
-      let points: PlotPoints = self.loss
-        .iter()
-        .enumerate()
-        .map(|(i, v)| [i as f64, *v])
-        .collect::<Vec<_>>()
-        .into();
-
-      Plot::new("loss_plot").show(ui, |plot_ui| {
-        plot_ui.line(Line::new(points));
-      });
-    });
-
-    ctx.request_repaint(); // ← essentiel pour rafraîchir en continu
-  }
-}
-
-fn get_app() -> App {
-  let app = App { x: vec![], y: vec![] };
-  eframe::run_native(
-    "Training Plot",
-    eframe::NativeOptions::default(),
-    Box::new(|_| Box::new(app)),
-  );
-
-  app
-}
 
 fn get_device() -> Device {
   // #[cfg(feature = "cuda")]
@@ -65,12 +29,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
   let fan_in = train_data.dim(0)?;
   let fan_out = train_label.dim(0)?;
 
-  let (_losses, _accs) = Network::new(fan_in, fan_out, Activation::Softmax, &device)
+  let (losses, accuracies) = Network::new(fan_in, fan_out, Activation::Softmax, &device)
     .add_layer(32, Activation::ReLU)
     .add_layer(32, Activation::ReLU)
     .build()?
     .quiet(true)
     .train(&train_data, &train_label, learning_rate, nb_iter)?;
+
+  if !losses.is_empty() || !accuracies.is_empty() {
+    frame::show_metrics_window(losses, accuracies)?;
+  }
 
   // println!("Retrieve test data from CSV file");
   // let test_data = dataset::get_test(&device)?;
